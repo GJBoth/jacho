69719b6b9426432fbdd53cee4d86174863e52dfc
Gert-Jan <gert-jan.both@cri-paris.org>
2021-08-04 20:01:33 +0200

    Fixed bug in ridge_training.

--- src/jacho/training/training.py
+++ src/jacho/training/training.py
@@ -2,20 +2,24 @@ from flax.core import unfreeze
 import jax.numpy as jnp
 from jax.scipy.linalg import cho_factor, cho_solve
 from flax.core import freeze, unfreeze
+import jax
 
 
-def ridge(model, params, state, u_train, renorm_factor=1.0, alpha=1e-2):
+def ridge(model, params, state, u_train, renorm_factor=1.0, alpha=1e-2, n_init=50):
     # Running reservoir and constructing feature matrix
     updated_state, intermediate = model.apply(
         params, state, u_train[:-1], method=model.run_reservoir
     )
-    X = jnp.concatenate([intermediate, renorm_factor * u_train[1:]], axis=-1).squeeze()
-    y = u_train[1:].squeeze()
+    X = jnp.concatenate(
+        [intermediate[n_init - 1 :], renorm_factor * u_train[n_init - 1 : -1]], axis=-1
+    ).squeeze()
 
     # Solving for output kernel
     # TODO: Check why this is pretty slow?
-    c, low = cho_factor(jnp.dot(X.T, X) + alpha * jnp.eye(X.shape[1]))
-    W_out = cho_solve((c, low), jnp.dot(X.T, y))
+    c, low = cho_factor(
+        jax.ops.index_add(jnp.dot(X.T, X), jnp.diag_indices(X.shape[1]), alpha)
+    )
+    W_out = cho_solve((c, low), jnp.dot(X.T, u_train[n_init:].squeeze()))
 
     # Updating outputlayer in parameters
     # TODO: Change to jax traverse_util
